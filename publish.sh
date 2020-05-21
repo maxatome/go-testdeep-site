87988eb8b0d6fc6362bc6c686dc22afc5327d2c7
Maxime Soulé <btik-git@scoubidou.com>
2020-05-21 17:44:30 +0200

    docs: publish go-testdeep.zetta.rocks
    Signed-off-by: Maxime Soulé <btik-git@scoubidou.com>

--- publish.sh
+++ publish.sh
@@ -3,9 +3,12 @@
 cd docs_src
 hugo -d ../docs --minify
 
-if git diff; then
+trap "rm -f /tmp/publish-$$.diff" EXIT INT
+
+if git diff --exit-code --color=always > publish-$$.diff; then
     echo "No change detected after hugo generation"
 else
+    less -R publish-$$.diff
     read -p "Do you want to commit these changes [y/N]: " ans
     case "$ans" in
         [yY]) git commit -a ;;
