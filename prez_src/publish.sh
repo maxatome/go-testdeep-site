5c83ce100ef5b4be55113db1612200f8934757ed
Maxime Soulé <btik-git@scoubidou.com>
2025-08-19 12:24:31 +0200

    fix(prez): /prez was deleted when theme changed, restore it
    Signed-off-by: Maxime Soulé <btik-git@scoubidou.com>

--- prez_src/publish.sh
+++ prez_src/publish.sh
@@ -1,20 +1,32 @@
 #!/bin/sh -e
 
+set -o pipefail
+
+cd "$(dirname "$0")"
+
 HOST=http://127.0.0.1:3999
 
-TARGET="$(dirname $0)/../docs/prez"
+present &
+trap "kill $!" EXIT INT
+sleep 1
 
-curl -s $HOST/static/styles.css |
-    perl -pE 'BEGIN { undef $/ } s,^ +/\* Add explicit links \*/.*?\n\n,,smg' > $TARGET/static/styles.css
+TARGET="$(dirname $0)/../docs/prez"
+rm -rf "$TARGET"
+
+mkdir -p "$TARGET/static"
+
+curl -s "$HOST/static/styles.css" |
+    perl -0777 -pE 's,^ +/\* Add explicit links \*/.*?\n\n,,smg' > "$TARGET/static/styles.css"
 
 for i in *.svg; do
-    [ $i != colored-output.svg -a $i != logo.svg ] &&
-        curl -s $HOST/$i > $TARGET/../images/$i
+    [ "$i" != colored-output.svg -a "$i" != logo.svg ] &&
+        curl -s "$HOST/$i" > "$TARGET/../images/$i"
 done
 
-curl -s $HOST/static/slides.js | sed -E 's/\/(static\/)/\1/' > $TARGET/static/slides.js
-curl -s $HOST/go-testdeep.slide |
-    sed -E 's/\/(static\/)/\1/' |
+curl -s "$HOST/static/slides.js" | sed -E 's,/(static/),\1,' > "$TARGET/static/slides.js"
+curl -s "$HOST/go-testdeep.slide" |
+    sed -E 's,/(static/),\1,' |
     sed -E '/play\.js/d' |
     sed -E '/ +$/d' |
-    sed -E 's/([-a-zA-Z0-9]+\.svg)/..\/images\/\1/' > $TARGET/index.html
+    sed -E 's,(<head>),\1<link href=/images/favicon.png rel=icon type=image/png>,' |
+    sed -E 's,([-a-zA-Z0-9]+\.svg),../images/\1,' > "$TARGET/index.html"
--- publish.sh
+++ publish.sh
@@ -6,6 +6,9 @@ cd "$dir"
 find docs -not -path docs \
           -not -path docs/CNAME \
           -not -path docs/.nojekyll \
+          -not -path docs/prez \
+          -not -path docs/images/enlarge-your-tests.svg \
+          -not -path docs/images/logo-rev.svg \
           -delete
 
 cd docs_src
