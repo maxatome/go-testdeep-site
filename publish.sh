7ab962a32efadfdea7fdc1adbcd9726ecedc5a3e
Maxime Soulé <btik-git@scoubidou.com>
2025-08-24 22:33:27 +0200

    fix(prez): /prez was deleted (again), restore it
    Signed-off-by: Maxime Soulé <btik-git@scoubidou.com>

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ cd "$dir"
 find docs -not -path docs \
           -not -path docs/CNAME \
           -not -path docs/.nojekyll \
-          -not -path docs/prez \
+          -not -path 'docs/prez/*' \
           -not -path docs/images/enlarge-your-tests.svg \
           -not -path docs/images/logo-rev.svg \
           -delete
